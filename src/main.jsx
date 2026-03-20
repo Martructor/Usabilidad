7229406887f2c5f35057783ffc723e91c922955b
Pedrus-afk <ptevarmartinez@gmail.com>
2026-03-20 08:55:47 +0100

    Funciona la pagina en local(localhost)

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Usabilidad">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AccessibilityProvider>
         <App />
       </AccessibilityProvider>
