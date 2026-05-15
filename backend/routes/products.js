e0c93841972da64e30b661d1c5f3c41252ea8a09
Martructor <71226504+Martructor@users.noreply.github.com>
2026-05-15 17:12:10 +0200

    Correccion fotos

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { Item } from '../models/Item.js';
 import { Farmacia } from '../models/Farmacia.js';
 import { Comentario } from '../models/Comentario.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/')
+    cb(null, path.join(__dirname, '../uploads/'))
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
